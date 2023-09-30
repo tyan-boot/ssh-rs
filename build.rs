667253a6d0cac9e679e9d75331be3f19ef3ccc5a
Tyan Boot <tyanboot@outlook.com>
2023-09-30 20:23:46 +0800

    add vcpkg for windows

--- build.rs
+++ build.rs
@@ -1,9 +1,46 @@
 fn main() {
+    let is_static = std::env::var("LIBSSH_STATIC").is_ok();
+
+    #[cfg(windows)]
+    if try_vcpkg(is_static) {
+        return;
+    }
+
+    #[cfg(not(windows))]
+    if try_pkg_config(is_static) {
+        return;
+    }
+
+    panic!("couldn't find libssh with specified options");
+}
+
+#[cfg(windows)]
+fn try_vcpkg(is_static: bool) -> bool {
+    if !is_static {
+        std::env::set_var("VCPKGRS_DYNAMIC", "true");
+    }
+
+    let lib = vcpkg::find_package("libssh");
+
+    match lib {
+        Ok(lib) => {
+            for metadata in lib.cargo_metadata {
+                println!("{}", metadata);
+            }
+
+            true
+        }
+        Err(_) => false,
+    }
+}
+
+#[cfg(not(windows))]
+fn try_pkg_config(is_static: bool) -> bool {
     // Dynamic libssh doesn't need initialization since 0.8,
     // so we require that to keep things easy.
     pkg_config::Config::new()
         .atleast_version("0.8")
-        .statik(false)
+        .statik(is_static)
         .probe("libssh")
-        .expect("dynamically linked libssh >= 0.8 is required");
+        .is_ok()
 }
